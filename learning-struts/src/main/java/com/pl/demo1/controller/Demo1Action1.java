ab05b78721ef0baf92ec88aff8fa47365e34564a
li.peng <li.peng@pengli.gticom.cn>
2018-08-13 11:10:47 +0800

    learning-struts的第一个例子demo1

---
+++ learning-struts/src/main/java/com/pl/demo1/controller/Demo1Action1.java
@@ -0,0 +1,14 @@
+package com.pl.demo1.controller;
+
+/**
+ * action动作类
+ * 
+ * @author li.peng
+ *
+ */
+public class Demo1Action1 {
+
+	public String hello() {
+		return "success";
+	}
+}
---
+++ learning-struts/src/main/java/com/pl/demo1/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * demo1就是学习struts框架最简单的helloworld
+ * <br/>
+ * 这个包放的是demo1的controller
+ * @author 彭立
+ *
+ */
+package com.pl.demo1.controller;
