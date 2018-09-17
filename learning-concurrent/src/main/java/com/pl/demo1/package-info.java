4133cff1398da74892fbb2c3170d2d00f2eab752
pengli4coding <pengli4coding@163.com>
2018-09-17 09:36:49 +0800

    modify

---
+++ learning-concurrent/src/main/java/com/pl/demo1/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * @Description:
+ * @author 彭立
+ * @date: 2018-09-17 09:35:53
+ *
+ */
+package com.pl.demo1;
+
+/**
+ * 
+ * 
+ * 
+ */
---
+++ learning-concurrent/src/test/java/com/pl/test/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @Description:
+ * @author 910146
+ * @date: 2018-09-17 09:36:30
+ *
+ */
+package com.pl.test;
